6b7c5a0c8be2f225fec8aeef7cfed545128cf7d6
MalekBael <87453158+MalekBael@users.noreply.github.com>
2021-07-15 21:42:48 +0200

    update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import datetime
 
 client = discord.Client()
 
-distoken = "865273307984298014"
+distoken = 865273307984298014
 
 # These must all be Voice Channels
 timechannel = 865229417877340161 # The ID of the Channel that gets renamed
@@ -16,4 +16,4 @@ async def on_ready():
         now = datetime.datetime.now()
         await client.get_channel(timechannel).edit(name=f"{now.hour}:{now.minute} (<GB>)") # The channel gets changed here
         await asyncio.sleep(60)
-client.run(865273307984298014)
+client.run(distoken)
